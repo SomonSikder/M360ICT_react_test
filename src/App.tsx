3e27d12b315d3e36e1842fbdb44ef3216708b3d9
SomonSikder <somonsikder777@gmail.com>
2022-11-25 14:48:35 +0600

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-info">Hello from TypeScrip and react</h1>
+    </div>
+  );
+}
+
+export default App;
